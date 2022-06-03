aca057350346601bb6896f6cdc01b92b70bd11fb
hacks-and-codes <owusuansahkwadwo24@gmail.com>
2022-06-03 08:56:07 +0000

    modified the signup page and the styles css file

---
+++ functionality.js
@@ -0,0 +1,9 @@
+function searrch(){
+    var searching = document.getElementById('searchbar');
+    searching.innerHTML = `<input type = "search" name="search"
+    style = width:30%; border:none; border-radius:5px;
+    box-shadow:#333; margin:14px; padding:0.5em;
+    cursor:pointer; list-style=none`
+    searching.click();
+};
+
