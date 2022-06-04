e31ad5ea827caa9a427b9bd59763fe53887e87ca
Owusu Ansah K <54241472+hacks-and-codes@users.noreply.github.com>
2022-06-04 03:52:03 +0000

    Add files via upload

--- cyberprogramming/functionality.js
+++ cyberprogramming/functionality.js
@@ -1,2 +1,11 @@
-
-
+/* sign up form*/ 
+function formContainer(){
+    document.getElementsByClassName("signup-form").style.display = "block";
+}
+function openForm() {
+    document.getElementById("form").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("form").style.display = "none";
+  }
