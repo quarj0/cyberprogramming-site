f4240a137e9effb142a9e27eba10e128aafe4eb3
hacks-and-codes <owusuansahkwadwo24@gmail.com>
2022-06-07 07:12:15 +0000

    added about page and made somes changes in js file.

--- functionality.js
+++ functionality.js
@@ -1,6 +1,6 @@
 /* sign up form*/ 
 function formContainer(){
-    document.getElementsByClassName("signup-form").style.display = "block";
+    document.getElementById("form").style.display = "block";
 }
 function openForm() {
     document.getElementById("form").style.display = "block";
@@ -9,3 +9,45 @@ function openForm() {
   function closeForm() {
     document.getElementById("form").style.display = "none";
   }
+
+
+/*Reset form*/
+var ycodes = document.getElementById("ycodes");
+ycodes.addEventListener=forget();
+function forget(){
+  //document.createElement('div')
+  document.write("<h1>Hello and welcome</h1>")
+
+}
+function checkForm()
+{
+ var oldP=document.getElementById("oldP").value;
+ var newP=document.getElementById("newP").value;
+ var confirmP =document.getElementById("confirmP").value;
+
+ if(oldP!=""&&newP!=""&&confirmP!="")
+ {
+   if(oldP!=newP)
+   {
+     if(newP==confirmP)
+      {
+       return true;
+      }
+      else
+       {
+         alert("Confirm password is not same as you new password.");
+         return false;
+       }
+   }
+   else
+  {
+   alert(" This Is Your Old Password,Please Provide A New Password");
+   return false;
+  }
+ }
+ else
+ {
+  alert("All Fields Are Required");
+  return false;
+ }
+}
