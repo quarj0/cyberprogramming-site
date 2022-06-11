a4ea2ba5939b04832f8b1154f4815733f84d575f
hacks-and-codes <owusuansahkwadwo24@gmail.com>
2022-06-11 17:09:43 +0000

    More functionalities. Added a hamburger menu and changed the navigation bar

--- functionality.js
+++ functionality.js
@@ -1,4 +1,19 @@
+// hamburger menubar 
+const burger = document.querySelector(".burger")
+const menuSidebar = document.querySelector(".menu-sidebar")
+const closeMenu = document.querySelector(".close-menu")
+
+burger.addEventListener("click", function(){
+  menuSidebar.style.transform = "translate(0%)"
+})
+
+closeMenu.addEventListener("clcik", function(){
+  menuSidebar.style.transform = "translate(-100%)"
+})
+
+
 /* sign up form*/ 
+
 function formContainer(){
     document.getElementById("form").style.display = "block";
 }
@@ -12,13 +27,6 @@ function openForm() {
 
 
 /*Reset form*/
-var ycodes = document.getElementById("ycodes");
-ycodes.addEventListener=forget();
-function forget(){
-  //document.createElement('div')
-  document.write("<h1>Hello and welcome</h1>")
-
-}
 function checkForm()
 {
  var oldP=document.getElementById("oldP").value;
@@ -50,4 +58,25 @@ function checkForm()
   alert("All Fields Are Required");
   return false;
  }
+}
+
+
+function ycodes() {
+  var regex = /0-9/
+  var check = [0,1,2,3,4,5,6,7,8,9]
+  var firstname = document.getElementById("firstname").value;
+  var lastname = document.getElementById("lastname").value;
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
+
+  if (firstname.trim() == "" || lastname.trim() == "" || email.trim()== "" || password == "") {
+  alert("All fields required!");
+    return false;
+  }
+  else if (firstname == regex.test(check) || lastname == regex.test(check) || password <=7)
+  {
+    alert("Sorry! Password is too short or name is containing numerics.");
+    return false;
+  }
+  
 }
